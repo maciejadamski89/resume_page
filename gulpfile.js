51830f3cb7fbbd4702596923c182fc231b437b14
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-06-13 13:07:10 +0200

    Migration to tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,43 @@
-const { src, dest, parallel,series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const clean = require('gulp-clean');
-const concat = require('gulp-concat')
-const browser_sync = require('browser-sync').create();
-var gulp = require('gulp');
+const { src, dest, parallel, series } = require("gulp");
+const clean = require("gulp-clean");
+const concat = require("gulp-concat");
+const browser_sync = require("browser-sync").create();
+var gulp = require("gulp");
 
 function cleanTask() {
-  return src('./dest/*')
-  .pipe(clean())
-}
-
-function sassTask() {
-  return src('./src/sass/*')
-  .pipe(sass())
-  .pipe(concat('styles.css'))
-  .pipe(dest(('./dest')))
-  .pipe(browser_sync.stream())
+  return src("./public/*").pipe(clean());
 }
 
 function copyVendorTask() {
-  return src('./src/vendor/*')
-  .pipe(dest(('./dest')))
+  return src("./src/vendor/*").pipe(dest("./public"));
 }
 
 function copyAssetsTask() {
-  return src('./src/assets/**/*')
-  .pipe(dest(('./dest')))
+  return src("./src/assets/**/*").pipe(dest("./public"));
 }
 
 function JSTask() {
-  return src('./src/js/*')
-  .pipe(dest(('./dest')))
-  .pipe(browser_sync.stream())
+  return src("./src/js/*").pipe(dest("./public")).pipe(browser_sync.stream());
 }
 
 function HTMLTask() {
-  return src('./src/html/*')
-  .pipe(dest(('./dest')))
-  .pipe(browser_sync.stream())
+  return src("./src/html/*").pipe(dest("./public")).pipe(browser_sync.stream());
 }
 
-function watch(){
+function watch() {
   browser_sync.init({
     server: {
-      baseDir: './dest'
-    }
+      baseDir: "./public",
+    },
   });
-  gulp.watch('./src/sass/**/*.sass',sassTask);
-  gulp.watch('./src/html/**/*.html',HTMLTask).on('change', browser_sync.reload);
-  gulp.watch('./src/js/**/*.js',JSTask).on('change', browser_sync.reload);
-
+  gulp
+    .watch("./src/html/**/*.html", HTMLTask)
+    .on("change", browser_sync.reload);
+  gulp.watch("./src/js/**/*.js", JSTask).on("change", browser_sync.reload);
 }
 
-
-exports.default = series(cleanTask, parallel(HTMLTask, sassTask, copyAssetsTask,copyVendorTask,JSTask));
+exports.default = series(
+  cleanTask,
+  parallel(HTMLTask, copyAssetsTask, copyVendorTask, JSTask)
+);
 exports.watch = watch;
