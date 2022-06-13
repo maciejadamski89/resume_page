528d1ea82f56aebfd07fedd83d4981e7c7810c6b
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-06-13 15:13:15 +0200

    Work in progrss: implement tailwind to html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "10rem",
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
